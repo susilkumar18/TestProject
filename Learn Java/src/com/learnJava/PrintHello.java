dd3dcfb53099e62980334e11139c692b3c92979a
susil <susil@DESKTOP-VM24ARN.home>
2018-11-04 21:05:50 -0500

    Commiting first Instance

---
+++ Learn Java/src/com/learnJava/PrintHello.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.learnJava;
+
+/**
+ * @author susil
+ *
+ */
+public class PrintHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("HelloWorld");
+			
+			PrintHelloBrother newObj = new PrintHelloBrother();
+			newObj.printName();
+	}
+
+}
+
---
+++ Learn Java/src/com/learnJava/PrintHelloBrother.java
@@ -0,0 +1,11 @@
+package com.learnJava;
+
+public class PrintHelloBrother {
+String name = new String ("susil");
+	
+	void printName()
+	{
+		System.out.println("Hi my name is "+ name);
+	}
+	
+}
